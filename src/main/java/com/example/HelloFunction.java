008faa55fc1efaa2036faa03fc54da11852f6f35
Max Hohenegger <eclipse@hohenegger.eu>
2021-09-25 20:11:10 +0200

    [WIP] Access external resource.

--- src/main/java/com/example/HelloFunction.java
+++ src/main/java/com/example/HelloFunction.java
@@ -1,16 +1,43 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.function.Function;
+
 import com.example.model.Greeting;
 import com.example.model.User;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
-import java.util.function.Function;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class HelloFunction implements Function<Mono<User>, Mono<Greeting>> {
 
     public Mono<Greeting> apply(Mono<User> mono) {
-        return mono.map(user -> new Greeting("Hello, " + user.getName() + "!\n"));
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+              .uri(URI.create("https://rest.ensembl.org/info/ping?content-type=application/json"))
+              .build();
+        // client.sendAsync(request, BodyHandlers.ofString())
+        // .thenApply(HttpResponse::body)
+        // .thenAccept(System.out::println)
+        // .join();
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "request failed", e);
+        }
+
+        return mono.map(user -> new Greeting("Hello, " + user.getName() +
+            "!\nExternal HTTP Request Reponse: " + response.body()));
     }
 }
